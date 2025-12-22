a3863c439d7f76a37db01b56973bd6f0f6c2a8b3
danicolasss <danny.grangeot@smff-magyar.com>
2025-12-22 15:54:31 +0100

    replace site title with logo in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-indigo-600">
-          Eurekadev
+          <img src="/eurekadevlogo.png" alt="Eurekadev" className="h-10 w-auto" />
         </Link>
 
         {/* Desktop Nav */}
