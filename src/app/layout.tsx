47ece0e99ae50407683ff79339558a21026b5969
danicolasss <danny.grangeot@smff-magyar.com>
2025-12-29 15:44:54 +0100

    replace Google Analytics script with new component from @next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -37,19 +37,7 @@ export default function RootLayout({
     <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
     >
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-YXLWWR34YE"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-YXLWWR34YE');
-        `}
-      </Script>
+      <GoogleAnalytics gaId="G-YXLWWR34YE" />
     <div className="bg-yellow-400 text-yellow-900 text-center py-2 px-4 font-medium text-sm">
       🚧 Site en construction - Certaines fonctionnalités peuvent être limitées. 🚧
         </div>
